cb85140dd2c5e30daad55861dc9a5f1a26d0ef88
lindenb <plindenbaum@yahoo.fr>
2020-07-23 12:35:30 +0200

    cont

--- hello02/app/src/main/java/com/github/lindenb/android/apps/hello02/MainActivity.java
+++ hello02/app/src/main/java/com/github/lindenb/android/apps/hello02/MainActivity.java
@@ -2,13 +2,25 @@ package com.github.lindenb.android.apps.hello02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.github.lindenb.android.apps.hello02.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+   public void  sendMessage(final View view) {
+       Intent intent = new Intent(this, DisplayMessageActivity.class);
+       EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+       String message = editText.getText().toString();
+       intent.putExtra(EXTRA_MESSAGE, message);
+       startActivity(intent);
     }
 }
