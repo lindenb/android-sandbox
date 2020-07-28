72a92e174f30025b5cc63f5f4d57cd9000af7008
lindenb <plindenbaum@yahoo.fr>
2020-07-28 15:04:32 +0200

    cont

--- drawinggrid/app/src/main/java/com/github/lindenb/android/apps/drawinggrid/DrawGridView.java
+++ drawinggrid/app/src/main/java/com/github/lindenb/android/apps/drawinggrid/DrawGridView.java
@@ -1,25 +1,149 @@
 package com.github.lindenb.android.apps.drawinggrid;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 public class DrawGridView extends View {
+    private boolean squareGrid=false;
+    private int numDiv = 8;
+    private Bitmap bitmap = null;
 
-    DrawGridView(final Context ctx) {
-        super(ctx);
+    public DrawGridView(final Context ctx, android.util.AttributeSet attributeSet) {
+        super(ctx,attributeSet);
     }
+
+    private boolean isPortrait(final int w,final int h) {
+        return h>w;
+    }
+
+    private Matrix getTransform() {
+        if(bitmap==null) return null;
+        int panelWidth = this.getWidth();
+        int panelHeight = this.getHeight();
+        int imgWidth = this.bitmap.getWidth();
+        int imgHeight = this.bitmap.getHeight();
+        if(imgWidth==0 || imgWidth==0 || panelWidth==0 || panelHeight==0) return null;
+        Matrix tr= new Matrix();
+
+        if( isPortrait(panelWidth, panelHeight) != isPortrait(imgWidth,imgHeight)) {
+            tr.postTranslate(imgHeight, 0);
+            tr.postRotate((float)(Math.PI/2));
+            int tmp = imgHeight;
+            imgHeight = imgWidth;
+            imgWidth = tmp;
+        }
+
+        if(imgWidth!= panelWidth || imgHeight!=panelHeight) {
+            final double r1 = (double)panelWidth/imgWidth;
+            final double r2 = (double)panelHeight/imgHeight;
+            final double r = Math.min(r1, r2);
+            final Matrix tr2 = new Matrix();
+            tr2.postTranslate((float)r, (float)r);
+            tr2.postConcat(tr);
+            tr=tr2;
+            imgWidth = (int)(imgWidth*r);
+            imgHeight =(int)(imgHeight*r);
+        }
+
+        final int dx=(panelWidth-imgWidth)/2;
+        final int dy=(panelHeight-imgHeight)/2;
+        final Matrix tr3 = new Matrix();
+        tr3.postTranslate(dx,dy);
+        ;
+        tr3.postConcat(tr);
+        tr=tr3;
+        return tr;
+    }
+
+
 
     @Override
     protected void onDraw(final Canvas canvas) {
         super.onDraw(canvas);
-        Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        canvas.drawRect(10,10,100,150,paint);
-        paint.setColor(Color.RED);
-        canvas.drawLine(15,15,100,150,paint);
+
+        final Paint paint = new Paint();
+        paint.setColor(this.bitmap==null?Color.DKGRAY:Color.LTGRAY);
+        canvas.drawRect(0, 0, getWidth(), getHeight(), paint);
+
+
+
+        Rect bounds = new Rect(0,0,getWidth(),getHeight());
+
+        boolean needRotate = false;
+
+        if(this.bitmap!=null) {
+            Matrix matrix = getMatrix();
+            if(matrix!=null) canvas.drawBitmap(this.bitmap,matrix,paint);
+        }
+
+
+        if (numDiv > 0) {
+            float dw = bounds.width() / (float) numDiv;
+            float dh = bounds.height() / (float) numDiv;
+            if (this.squareGrid) {
+                dw = Math.min(dw, dh);
+                dh = dw;
+                }
+
+
+
+            int i=1;
+            float p = bounds.left  + dw;
+            while(p < bounds.right) {
+                if(i%2==0) {
+                    paint.setStrokeWidth(2f);
+                    paint.setColor(Color.RED);
+                }
+                else
+                {
+                    paint.setStrokeWidth(1f);
+                    paint.setColor(Color.BLUE);
+                }
+                canvas.drawLine(p, bounds.top,p, bounds.bottom,paint);
+                p+= dw;
+                i++;
+            }
+
+            i=1;
+            p= bounds.top + dh;
+            while(p < bounds.bottom) {
+                if(i%2==0) {
+                    paint.setStrokeWidth(2f);
+                    paint.setColor(Color.RED);
+                }
+                else
+                {
+                    paint.setStrokeWidth(1);
+                    paint.setColor(Color.BLUE);
+                }
+                canvas.drawLine(bounds.left , p, bounds.right,p,paint);
+                p+= dh;
+                i++;
+            }
+        }
     }
 
+    void setNumDiv(int numDiv) {
+        if(numDiv==this.numDiv) return;
+        this.numDiv = numDiv;
+        invalidate();
+    }
+
+    void setBitmap(final Bitmap bitmap) {
+        if(this.bitmap==null && bitmap==null) return;
+        this.bitmap = bitmap;
+        invalidate();
+    }
+
+    void setSquareGrid(boolean squareGrid) {
+        if(this.squareGrid == squareGrid) return;
+        this.squareGrid = squareGrid;
+        invalidate();
+    }
 }
--- drawinggrid/app/src/main/java/com/github/lindenb/android/apps/drawinggrid/MainActivity.java
+++ drawinggrid/app/src/main/java/com/github/lindenb/android/apps/drawinggrid/MainActivity.java
@@ -3,32 +3,62 @@ package com.github.lindenb.android.apps.drawinggrid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
-    private final static int PICK_IMAGE=1;
-    private Uri currImageURI= null;
+    private final static int PICK_IMAGE=101;
+    private final static String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button buttonOpenImage = (Button) findViewById(R.id.selFileButton);
-        buttonOpenImage.setOnClickListener(new View.OnClickListener() {
+        // https://stackoverflow.com/questions/36236181
+        try { this.getSupportActionBar().hide(); }  catch (Throwable e){   Log.w(TAG,e);  }
+
+        final Button buttonOpenImage = findViewById(R.id.selFileButton);
+
+        final CheckBox cboxSquare = findViewById(R.id.squareCbox);
+        cboxSquare.setOnCheckedChangeListener( new CheckBox.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                final Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
-                }
-            });
-        final Button showGridButton =  (Button) findViewById(R.id.showGridButton);
-        final CheckBox cboxSquare = (CheckBox)findViewById(R.id.rotateCbox);
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                getDrawingArea().setSquareGrid(b);
+            }
+        });
+
+        final SeekBar gridCountSeekBar = findViewById(R.id.gridCount);
+        gridCountSeekBar.setMax(20);
+        gridCountSeekBar.setProgress(8);
+        gridCountSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d(TAG,"Div:"+progress);
+                cboxSquare.setText(""+progress);
+                getDrawingArea().setNumDiv(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
 
 
         // recovering the instance state
@@ -46,14 +76,54 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
 
         if (resultCode == RESULT_OK) {
 
-            if (requestCode == PICK_IMAGE) {
-                currImageURI = data.getData();
+            if (requestCode == PICK_IMAGE ) {
+                Log.d(TAG,"PickImage");
+                final Uri pickedImage = data.getData();
+                if(pickedImage!=null) {
+
+                    Log.d(TAG,"PickImage is not null");
+                    final String[] filePath = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = null;
+                    try {
+                        cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
+                        if (cursor != null && cursor.moveToFirst()) {
+                            final String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+                            Log.d(TAG,"imagePath:"+imagePath);
+                            BitmapFactory.Options options = new BitmapFactory.Options();
+                            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                            final Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
+                            Log.d(TAG,"Update bit map:"+(bitmap==null?"null":"ok"));
+                            getDrawingArea().setBitmap(bitmap);
+                        }
+
+                    } catch (final Throwable err) {
+                        Log.d(TAG, "Cannot read image",err);
+                    } finally {
+                        if (cursor != null) cursor.close();
+                    }
+                }
+
+
             }
         }
-        super.onActivityResult(requestCode,resultCode,data);
+    }
+
+
+    private DrawGridView getDrawingArea() {
+        return (DrawGridView)findViewById(R.id.drawingArea);
+    }
+
+
+    public void  chooseImage(final View view) {
+        Log.d(TAG,"Select image");
+        final Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
     }
 
 
